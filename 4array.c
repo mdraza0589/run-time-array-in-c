0be0e33174c16ddbec7421efdf8c16be599194ae
MOHAMMAD RAZA <razamohammad0589@gmail.com>
2025-03-01 21:03:57 +0530

    Add files via upload

---
+++ 4array.c
@@ -0,0 +1,37 @@
+// ****************************************** RUN TIME INITIALISATION
+
+
+
+#include<stdio.h>
+int main()
+{
+    int a[5];
+    printf("enter the five number= ");
+    scanf("%d%d%d%d%d",&a[0],&a[1],&a[2],&a[3],&a[4]);
+    printf("values are= %d",a[4]*4);
+}
+
+#include<stdio.h>
+int main()
+{
+    int a[11];
+    scanf("%d%d%d%d",&a[0],&a[2],&a[1],&a[3]);
+    printf("value is= %d",a[10]);
+}
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a[5],i;
+    
+//     for(i=0;i<5;i++)
+//     {
+//         printf("enter a number= ");
+//         scanf("%d",&a[i]);
+//     }
+//     scanf("%d",&a[i]);
+
+//     for(i=0;i<5;i++)
+//     printf("value are= %d\n",a[i]);
+
+// }
